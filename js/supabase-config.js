b3bf975b91f589bf6ac839dba5e71c903bec2581
FleetMagnify <fleetmagnify@gmail.com>
2026-06-14 21:13:01 +1200

    Add login.html

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 // Replace with your Supabase project credentials from:
 // https://supabase.com/dashboard/project/_/settings/api
 window.SUPABASE_URL = 'https://pddsgvuzvuwueuvpoytw.supabase.co';
-window.SUPABASE_ANON_KEY = 'paste your anon key hereeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkZHNndnV6dnV3dWV1dnBveXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNzQxOTQsImV4cCI6MjA5Njk1MDE5NH0.iTofb-BGv_Pdqsipw3YtoRs2m3ejmtRFPtA0CfCgA9Q';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkZHNndnV6dnV3dWV1dnBveXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNzQxOTQsImV4cCI6MjA5Njk1MDE5NH0.iTofb-BGv_Pdqsipw3YtoRs2m3ejmtRFPtA0CfCgA9Q';
